ef1b238424eade8021e191db182a1a67a54ca37b
juhyeon <juhyeon1114@gmail.com>
2023-11-23 22:35:05 +0900

    Servlet

--- src/main/java/com/example/helloboot/HellobootApplication.java
+++ src/main/java/com/example/helloboot/HellobootApplication.java
@@ -1,13 +1,79 @@
 package com.example.helloboot;
 
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class HellobootApplication {
 
+//    public static void main(String[] args) {
+//        SpringApplication.run(HellobootApplication.class, args);
+//    }
+
+    /**
+     * #1 Servlet container
+     */
+//    public static void main(String[] args) {
+//        TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+//        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+//            servletContext.addServlet("hello", new HttpServlet() {
+//                @Override
+//                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//                    String name = req.getParameter("name");
+//
+//                    resp.setStatus(HttpStatus.OK.value());
+//                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+//                    resp.getWriter().println("Hello Servlet " + name);
+//                }
+//            }).addMapping("/hello");
+//        });
+//        webServer.start();
+//    }
+
+    /**
+     * #2 Front controller
+     */
     public static void main(String[] args) {
-        SpringApplication.run(HellobootApplication.class, args);
+        TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            HelloController helloController = new HelloController();
+
+            servletContext.addServlet("frontcontroller", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    // 인증, 보안, 다국어, 공통 기능
+                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+                        String name = req.getParameter("name");
+
+                        String hello = helloController.hello(name);
+
+                        resp.setStatus(HttpStatus.OK.value());
+                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        resp.getWriter().println(hello);
+                    } else if (req.getRequestURI().equals("/user")) {
+                        //
+                    } else {
+                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+                    }
+
+                }
+            }).addMapping("/*");
+        });
+        webServer.start();
     }
 
 }
